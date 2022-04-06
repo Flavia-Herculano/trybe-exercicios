e6953f293af3a449b9da1489fdeb65297a376919
Flávia Herculano <flaviacunha18@hotmail.com>
2022-04-05 21:31:31 -0300

    Adiciona o teste 0 mais 0 é igual a 0

--- dia-3-primeiros-passos-em-jest/sum.test.js
+++ dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,12 @@
 const sum = require('./sum');
 
+//1 - A função sum(a, b) retorna a soma do parâmetro a com o b
+// 1.1 Teste se o retorno de sum(4, 5) é 9
 test('soma dois valores', () => {
     expect(sum(4, 5)).toBe(9);
+});
+
+//1.2 Teste se o retorno de sum(0, 0) é 0
+test('0 mais 0 é igual a 0', () => {
+    expect(sum(0, 0)).toBe(0);
 });
