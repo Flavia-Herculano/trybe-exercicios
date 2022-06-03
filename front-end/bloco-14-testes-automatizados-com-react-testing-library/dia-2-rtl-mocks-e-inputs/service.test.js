152fcb6f1c13b8c5b9a9bfd496aa56c8efb1dee9
Flávia Herculano <flaviacunha18@hotmail.com>
2022-06-03 01:00:06 -0300

    Resolve o 3º item da 1ª Parte

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/service.test.js
@@ -1,6 +1,6 @@
 let retornaNumeroAleatorio = require('./service');
 
-test('Verifica se a função retornaNumeroAleatorio() foi chamada', () => {
+test('1 - Verifica se a função retornaNumeroAleatorio() foi chamada', () => {
 retornaNumeroAleatorio = jest.fn();
 retornaNumeroAleatorio();
 expect(retornaNumeroAleatorio).toHaveBeenCalled();
@@ -15,7 +15,7 @@ test('Verifica quantas vezes a função foi chamada', () => {
     expect(retornaNumeroAleatorio).toHaveBeenCalledTimes(1);
 });
 
-test('Verifica se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros', () => {
+test('2 - Verifica se a função foi chamada, qual seu retorno, quantas vezes foi chamada e com quais parâmetros', () => {
     retornaNumeroAleatorio = jest.fn().mockImplementation((a, b) => a / b);
     retornaNumeroAleatorio();
 
@@ -23,5 +23,20 @@ test('Verifica se a função foi chamada, qual seu retorno, quantas vezes foi ch
     expect(retornaNumeroAleatorio).toHaveBeenCalled();
     expect(retornaNumeroAleatorio(10, 5)).toBe(2);
     expect(retornaNumeroAleatorio).toHaveBeenCalledWith(10, 5);
+});
 
+describe('3 - testando implementações',() =>{
+    test('Verifica se a função mocakada recebe 3 parâmetros e retorna sua multiplicação', () => {
+        retornaNumeroAleatorio = jest.fn().mockImplementation((a, b, c) => a * b * c);
+
+        expect(retornaNumeroAleatorio(3, 5, 2)).toBe(30);
+        expect(retornaNumeroAleatorio).toHaveBeenCalledWith(3, 5, 2);
+    });
+    test('Verifica se a função mokada recebe um parâmetro e retorna seu dobro', () => {
+        retornaNumeroAleatorio = jest.fn().mockReset();
+        retornaNumeroAleatorio = jest.fn().mockImplementation((a) => a * 2);
+        
+        expect(retornaNumeroAleatorio(6)).toBe(12);
+        expect(retornaNumeroAleatorio).toHaveBeenCalledWith(6);
+    });
 });
